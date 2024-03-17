b2132563263a82e0be20482bbe5d136e7c357663
SaravanaPriya31 <Saravana31>
2024-03-18 02:17:09 +0530

    865587: commit

--- How to/Open and Close bookmark pane/index.js
+++ How to/Open and Close bookmark pane/index.js
@@ -3,7 +3,7 @@ var pdfviewer = new ej.pdfviewer.PdfViewer({
     resourceUrl:'https://cdn.syncfusion.com/ej2/24.2.3/dist/ej2-pdfviewer-lib'
 });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
-                              ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields);
+                              ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields, ej.pdfviewer.PageOrganizer);
 document.getElementById('closeBookmark').addEventListener('click', () => {
   // close Bookmark pane
   pdfviewer.bookmarkViewModule.closeBookmarkPane();
--- How to/Organize pdf/index.js
+++ How to/Organize pdf/index.js
@@ -1,5 +1,5 @@
 var pdfviewer = new ej.pdfviewer.PdfViewer({
-    documentPath: 'https://cdn.syncfusion.com/content/pdf/form-designer.pdf',
+    documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
     resourceUrl:'https://cdn.syncfusion.com/ej2/24.2.3/dist/ej2-pdfviewer-lib'
 });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
