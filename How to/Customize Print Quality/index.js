082d29db4735da380e4dced91a69cd72d97b37df
Vincentsf4657 <vincentkumar.l@syncfusion.com>
2024-09-20 07:37:47 +0530

    906190: UGDocumentation

--- How to/Customize Print Quality/index.js
+++ How to/Customize Print Quality/index.js
@@ -5,6 +5,7 @@ var pdfviewer = new ej.pdfviewer.PdfViewer({
      serviceUrl: 'https://services.syncfusion.com/js/production/api/pdfviewer',
  });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.Toolbar, ej.pdfviewer.Magnification, ej.pdfviewer.LinkAnnotation,ej.pdfviewer.ThumbnailView, ej.pdfviewer.BookmarkView, ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Navigation, ej.pdfviewer.Print);
+//By adjusting the printScaleFactor value we can adjust the quality of the  PDF file.
 pdfviewer.printScaleFactor = 0.5;
 pdfviewer.appendTo('#PdfViewer');
 pdfviewer.toolbarClick = function (args) {
