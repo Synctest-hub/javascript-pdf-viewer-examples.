880ed7cfb0d166ff758a60f68bb5e0644ce39009
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2024-11-25 16:16:56 +0530

    381112: included comments

--- How to/Get Base 64 string of the loaded PDF document/index.js
+++ How to/Get Base 64 string of the loaded PDF document/index.js
@@ -1,7 +1,6 @@
 var pdfviewer = new ej.pdfviewer.PdfViewer({
   documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
-  //serviceUrl: 'https://services.syncfusion.com/js/production/api/pdfviewer',
-  resourceUrl: "https://cdn.syncfusion.com/ej2/23.2.6/dist/ej2-pdfviewer-lib",
+  resourceUrl: "https://cdn.syncfusion.com/ej2/27.2.2/dist/ej2-pdfviewer-lib",
 });
 ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
   ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields, ej.pdfviewer.PageOrganizer);
@@ -10,6 +9,8 @@ pdfviewer.appendTo('#PdfViewer');
 document.getElementById('getBase64').addEventListener('click', function() {
   base64ofloadedDocument();  // Call the function to get the Base64 string
 });
+
+// Function to get Base64 of the loaded document
 function base64ofloadedDocument() {
 pdfviewer.saveAsBlob().then(function(value) {
   var data = value;
