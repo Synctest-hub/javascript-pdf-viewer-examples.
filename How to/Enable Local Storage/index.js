652da718a714e705a56f21e143a8286ea522e710
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2024-12-13 12:21:22 +0530

    927839: Sample on How to Enable Local Storage

---
+++ How to/Enable Local Storage/index.js
@@ -0,0 +1,8 @@
+var pdfviewer = new ej.pdfviewer.PdfViewer({
+  documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
+  resourceUrl: "https://cdn.syncfusion.com/ej2/27.2.5/dist/ej2-pdfviewer-lib",
+});
+ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Print, ej.pdfviewer.Navigation, ej.pdfviewer.Toolbar,
+  ej.pdfviewer.Magnification, ej.pdfviewer.Annotation, ej.pdfviewer.FormDesigner, ej.pdfviewer.FormFields, ej.pdfviewer.PageOrganizer);
+  pdfviewer.enableLocalStorage=true; 
+  pdfviewer.appendTo('#PdfViewer');
